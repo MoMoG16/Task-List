4f8491207d6f1957cd9fd76be6f9124bd49704ac
Moira Glace <mglace16@gmail.com>
2016-03-11 09:39:36 -0600

    Add sign in and sign out for Facebook App

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+
+    #Create user using the hash that is returned from the Facebook API
+    def self.create_with_omniauth(auth)
+      create! do |user|
+        user.provider = auth["provider"]
+        user.uid = auth["uid"]
+        user.name = auth["info"]["name"]
+      end
+    end
+
+    def small_image
+      "http://graph.facebook.com/#{uid}/picture?type=small"
+    end
+
+    def normal_image
+      "http://graph.facebook.com/#{uid}/picture?type=normal"
+    end
+end
